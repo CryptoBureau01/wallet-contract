db5736d2b4c7cf3cb3d3b395898034625b36e32b
Crypto Bureau Master <152272581+CryptoBureau01@users.noreply.github.com>
2025-02-04 22:48:53 +0530

    Update deposit.sol

--- contract/deposit.sol
+++ contract/deposit.sol
@@ -2,15 +2,18 @@
 pragma solidity ^0.8.20;
 
 import "./token.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";  // Import Ownable contract
 
-contract Depo is Token {
+contract Depo is Token, Ownable {  // Inherit from Ownable to get the onlyOwner modifier
     event DepositedETH(address indexed owner, uint256 amount);
     event DepositedToken(address indexed token, address indexed owner, uint256 amount);
 
+    // Deposit ETH function with onlyOwner modifier inherited from Ownable
     function depositETH() external payable onlyOwner {
         emit DepositedETH(msg.sender, msg.value);
     }
 
+    // Deposit Token function with onlyOwner modifier inherited from Ownable
     function depositToken(address token, uint256 amount) external onlyOwner {
         require(allowedTokens[token], "Token not allowed");
         require(amount > 0, "Amount must be greater than 0");
