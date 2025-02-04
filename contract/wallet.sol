dd88f281006475139d37e7c64eee6624d11ff2a5
Crypto Bureau Master <152272581+CryptoBureau01@users.noreply.github.com>
2025-02-04 21:28:58 +0530

    Update wallet.sol

--- contract/wallet.sol
+++ contract/wallet.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "./token.sol";
+import "./depo.sol";
+import "./with.sol";
+import "./list.sol";
+
+contract Wallet is Token, Depo, With, List {
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+        // ETH is allowed by default, it will be in Token.sol
+    }
+
+    function BalanceList() external view onlyOwner returns (string[] memory) {
+        return getTokenList();  // From List.sol, fetch all token names
+    }
+}
