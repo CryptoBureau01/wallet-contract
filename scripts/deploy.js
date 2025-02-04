c76ee7430372ffe6b75cf6ea590937c3232cfaa8
Crypto Bureau Master <152272581+CryptoBureau01@users.noreply.github.com>
2025-02-04 22:24:54 +0530

    Create deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+// scripts/deploy.js
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Wallet = await ethers.getContractFactory("Wallet");
+  const wallet = await Wallet.deploy();
+  console.log("Wallet contract deployed to:", wallet.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
