7aaf702fb31c49889884fda80a09814e096415cf
Crypto Bureau Master <152272581+CryptoBureau01@users.noreply.github.com>
2025-02-06 14:27:07 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,15 @@
-require('@nomiclabs/hardhat-ethers');
-require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers'); 
 require('dotenv').config();  // Load environment variables from .env file
 
 module.exports = {
   solidity: "0.8.20",
   paths: {
     sources: "./contract",  // Yeh line add karein
-  },
+  },  
   networks: {
     sepoliaBase: {
       url: process.env.SEPOLIA_RPC_URL,  // Load from .env file
-      accounts: [process.env.PRIVATE_KEY],  // Load private key from .env file
+      accounts: [process.env.PRIVATE_KEY].filter(Boolean),  // Filter undefined values
     },
   },
 };
